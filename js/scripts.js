7de8f88a284c61768d0c5dde644423ea2841e720
eric <eric.kingoo2014@gmail.com>
2019-02-22 17:34:42 +0300

    add functional submit event

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,37 @@
 
 
-$(document).ready(function(){
-  $("form#quiz").submit(function(event){
+
+  $("form#javascr").submit(function(event){
+
+    var question1 = $("input:radio[name=radio1]:checked").val();
+    var question2 = $("input:radio[name=radio2]:checked").val();
+    var question3= $("input:radio[name=radio3]:checked").val();
+
+
+    // $("#display").text("Your score is:" + result +"/20 pts");
+
     event.preventDefault();
-    var radios =["1", "2","3","4"];
-     radios.forEach(function(eachRadio){
-
-         var oneRadio = $("input:radio[name=radio" + eachRadio+"]:checked").val();
-         $("#results").text(oneRadio);
-     });
-   });
-
-
 
   });
-});
+
+var yourScore=0
+
+function marks(){
+  if (question1 == "2") {
+    yourScore += 20;
+  }
+  if (question2 == "1") {
+    yourScore += 20;
+
+  }
+  if (question3 == "2") {
+    yourScore += 20;
+  }
+}
+
+
+
+
+
+
+document.getElementById("results").innerHTML = "Your score is " +yourScore;
