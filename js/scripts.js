b68bee5b705988f79464c71bebe989a610bacec3
eric <eric.kingoo2014@gmail.com>
2019-02-22 15:21:51 +0300

    add button functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
-$(document).ready(function(){
-  ("form#quiz").click(function(){
-    var radios =["radio1", "radio2","radio3","radio4","radio5","radio6","radio7","radio8","radio9","radio10","radio11"];
-    radios.forEach(function(radio){
-      var flavor = $("input:radio[name= radio]:checked").val();
-      
-    })
 
-  })
-})
+
+$(document).ready(function(){
+  $("form#quiz").click(function(event){
+    event.preventDefault();
+    var radios =["radio1", "radio2","radio3","radio4"];
+    radios.forEach(function(radio){
+      var oneRadio = $("input:radio[name= radio ]:checked").val();
+      $(".col-md-4").text(oneRadio);
+    });
+
+  });
+});
