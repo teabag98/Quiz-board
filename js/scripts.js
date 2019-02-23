cbd7981bffe568e6e4a57bc36aaec6dbe7d08974
eric <eric.kingoo2014@gmail.com>
2019-02-23 08:52:54 +0300

    add column

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,10 @@
 
+//user logic
+$(document).ready(function(){
 
+$("button#submit").click(function(){
+$(".col-md-4").append("You are a winnrknknknkknknknknknknknknknknknknknknknknknknkkknknekrglnelkrgnlenrglernglerng");
+});
 
-  $("form#javascr").submit(function(event){
-
-    var question1 = $("input:radio[name=radio1]:checked").val();
-    var question2 = $("input:radio[name=radio2]:checked").val();
-    var question3= $("input:radio[name=radio3]:checked").val();
-
-
-    // $("#display").text("Your score is:" + result +"/20 pts");
-
-    event.preventDefault();
-
-  });
-
-var yourScore=0
-
-function marks(){
-  if (question1 == "2") {
-    yourScore += 20;
-  }
-  if (question2 == "1") {
-    yourScore += 20;
-
-  }
-  if (question3 == "2") {
-    yourScore += 20;
-  }
-}
-
-
-
-
-
-
-document.getElementById("results").innerHTML = "Your score is " +yourScore;
+});
+var input1=$("input:radio[name=radio1]:checked").val();
