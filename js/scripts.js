eff00bdaa33757faa636e31d2e257ddf3e51d625
eric <eric.kingoo2014@gmail.com>
2019-02-24 01:42:13 +0300

    add home html file

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ $("button#submit").click(function(){
   var input3=parseInt($("input:radio[name=radio3]:checked").val());
   var input4=parseInt($("input:radio[name=radio5]:checked").val());
   var results = eval(input1 + input2+input3+input4);
- $(".col-md-4").removeClass();
+ // $(".col-md-4").removeClass();
   $(".col-md-4").addClass("yourScore");
 $(".col-md-4").text("Your score is "+ results );
 });
@@ -17,9 +17,20 @@ $(".col-md-4").text("Your score is "+ results );
 
 
 
+//
 //business logic
-
 function messages(){
-  var allValues = ["input1","input2","input3","input4","input5"]
+  var output1 = document.javascr.radio1.value;
+  var output2 = document.javascr.radio2.value;
+  var output3 = document.javascr.radio3.value;
+  var output4 = document.javascr.radio4.value;
 
+
+if(output1== ""|| output2 ==""||output3==""||output4==""){
+alert()
+return false;
 }
+else{
+  return true;
+}
+};
