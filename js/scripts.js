aaef899eb1c97d5f2d8c5250150fc1fc6f88b03d
eric <eric.kingoo2014@gmail.com>
2019-02-24 13:57:35 +0300

    ft ok buttons working well

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,3 @@
-//user logic
-$(document).ready(function() {
-
-  $("button#submit").click(function() {
-    var input1 = parseInt($("input:radio[name=radio1]:checked").val());
-    var input2 = parseInt($("input:radio[name=radio2]:checked").val());
-    var input3 = parseInt($("input:radio[name=radio3]:checked").val());
-    var input4 = parseInt($("input:radio[name=radio5]:checked").val());
-    var results = eval(input1 + input2 + input3 + input4);
-    // $(".col-md-4").removeClass();
-    $(".col-md-4").addClass("yourScore");
-    $(".col-md-4").text("Your score is " + results);
-  });
-
-});
 
 
 
@@ -26,7 +11,7 @@ function messages() {
 
   if (output1 == "" || output2 == "" || output3 == "" || output4 == "") {
     //custom alert box
-    $(".box2").show("box2");
+    $(".box2").show(".box2");
 
     return false;
   } else {
@@ -34,3 +19,36 @@ function messages() {
     return true;
   }
 };
+
+
+
+
+
+
+
+
+
+
+
+//user logic
+$(document).ready(function() {
+  $("button#boxbutton1").click(function(){
+    $(".box").fadeOut();
+  })
+  $("button#boxbutton2").click(function(){
+    $(".box2").fadeOut();
+  })
+
+  $("button#submit").click(function() {
+    var input1 = parseInt($("input:radio[name=radio1]:checked").val());
+    var input2 = parseInt($("input:radio[name=radio2]:checked").val());
+    var input3 = parseInt($("input:radio[name=radio3]:checked").val());
+    var input4 = parseInt($("input:radio[name=radio5]:checked").val());
+    var results = eval(input1 + input2 + input3 + input4);
+    // $(".col-md-4").removeClass();
+    $(".col-md-4").addClass("yourScore");
+    $(".col-md-4").text("Your score is " + results);
+  });
+
+
+});
